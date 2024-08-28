bc74e44cab6947b9b6135129f10b7ab01496f65e
KennyPhilips <kennyphilipss@gmail.com>
2024-08-28 15:13:00 +0200

    Make function drawGrid() Make function hoverGrid() Make slider to adjust gridsize.

---
+++ javascript.js
@@ -0,0 +1,52 @@
+//config
+const gridSizeSlider = document.querySelector("#gridsize-slider");
+let gridSize = gridSizeSlider.value;
+const gridDimension = 600;
+const boxWidth = gridDimension / gridSize;
+const container = document.querySelector("#container");
+
+//drawGrid
+function drawGrid(){
+    for (let i = 0; i < gridSize; i++) {
+        const newLine = document.createElement("div");
+        newLine.setAttribute("class", "line");
+        container.appendChild(newLine);
+        for (let j = 0; j < gridSize; j++) {
+            const newBox = document.createElement("div");
+            newBox.setAttribute("class", "box");
+            newLine.appendChild(newBox);
+        }    
+    }
+}
+
+//hover effect
+function hoverEffect(){
+    const boxes = document.querySelectorAll("div.box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "red";
+        })
+    })
+    /*boxes.forEach((box) => {
+        box.addEventListener("mouseleave", () => {
+            box.style.backgroundColor = "";
+        })
+    })*/
+}
+
+//hide grid
+function hideGrid(){
+    container.innerHTML = "";
+}
+
+//execution
+drawGrid();
+hoverEffect();
+
+//resize grid
+gridSizeSlider.addEventListener("mouseup", () => {
+    gridSize = gridSizeSlider.value;
+    hideGrid();
+    drawGrid();
+    hoverEffect();
+})
